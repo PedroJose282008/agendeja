db048363b539dfc084bb6d8d907fc9711b648319
Pedro José <rm100799@estudante.fieb.edu.br>
2026-02-20 19:56:10 -0300

    primeiro commit guys

---
+++ src/main/java/com/agendamento/agendeja/AgendejaApplication.java
@@ -0,0 +1,13 @@
+package com.agendamento.agendeja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendejaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/agendamento/agendeja/model/entity/Usuario.java
@@ -0,0 +1,16 @@
+package com.agendamento.agendeja.model.entity;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String senha;
+    private String endeco;
+    private String area_atuacao;
+    private String cnpj;
+    private String cidade;
+    private String estado;
+
+}
